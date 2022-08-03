0555dc91d04da74fbb121c4ff415f76a65704cb6
Michael <navarro.michael93@gmail.com>
2022-08-02 20:27:26 -0500

    chapter 02 exercises

---
+++ Programming Principles and Practices Using C++/chap_02_drills.cpp
@@ -0,0 +1,14 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    // Drill #01
+    cout << "\nHello, World";
+    // keep_window_open();
+
+    // Exercise #01
+    cout << "\nHello, programming!";
+    cout << "\nHere we go!\n";
+
+    return 0;
+}
